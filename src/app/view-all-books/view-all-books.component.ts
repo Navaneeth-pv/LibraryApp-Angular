e59211211f2a4c6bcd27ce89b3f0515a269fcf61
Navaneeth-pv <navaneethpv902@gmail.com>
2022-11-28 08:27:48 +0530

    bug fixed

--- src/app/view-all-books/view-all-books.component.ts
+++ src/app/view-all-books/view-all-books.component.ts
@@ -6,5 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-all-books.component.css']
 })
 export class ViewAllBooksComponent {
+books : any=[
+  {
+    "title":"The Alchemist",
+    "image":"https://m.media-amazon.com/images/I/51FXs5gTmdL._SX427_BO1,204,203,200_.jpg",
+    "author":"Paulo Coulho",
+    "price":"550"
+  },
+  {
+    "title":"Strangr things",
+    "image":"https://kbimages1-a.akamaihd.net/ae21e88d-ed7a-4718-993b-47b730a94e26/353/569/90/False/stranger-things-the-bully-graphic-novel.jpg",
+    "author":"Palbert",
+    "price":"550"
+  },
+  {
+    "title":"Akbar",
+    "image":"https://m.media-amazon.com/images/I/81wrjtPzTNL.jpg",
+    "author":"shzam",
+    "price":"550"
+  },
+  {
+    "title":"The Alchemist",
+    "image":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1328847563l/965733.https://m.media-amazon.com/images/I/81Lfw-skUsL.jpg",
+    "author":"Paulo Coulho",
+    "price":"550"
+  },
+  {
+    "title":"The Girl in ice",
+    "image":"https://www.rd.com/wp-content/uploads/2021/10/girl-in-ice-ferencik-ecomm-via-amazon.jpg?fit=700,700",
+    "author":"Erica",
+    "price":"550"
+  },
+  {
+    "title":"Book of night",
+    "image":"https://www.rd.com/wp-content/uploads/2021/10/book-of-night-black-ecomm-via-amazon.jpg?fit=700,700",
+    "author":"holy",
+    "price":"550"
+  },
+  {
+    "title":"The Alchemist",
+    "image":"https://m.media-amazon.com/images/I/51FXs5gTmdL._SX427_BO1,204,203,200_.jpg",
+    "author":"Paulo Coulho",
+    "price":"550"
+  },
+  {
+    "title":"The Alchemist",
+    "image":"https://m.media-amazon.com/images/I/51FXs5gTmdL._SX427_BO1,204,203,200_.jpg",
+    "author":"Paulo Coulho",
+    "price":"550"
+  },
+  {
+    "title":"The Alchemist",
+    "image":"https://m.media-amazon.com/images/I/51FXs5gTmdL._SX427_BO1,204,203,200_.jpg",
+    "author":"Paulo Coulho",
+    "price":"550"
+  },
+  {
+    "title":"The Alchemist",
+    "image":"https://m.media-amazon.com/images/I/51FXs5gTmdL._SX427_BO1,204,203,200_.jpg",
+    "author":"Paulo Coulho",
+    "price":"550"
+  },
+  {
+    "title":"The Alchemist",
+    "image":"https://m.media-amazon.com/images/I/51FXs5gTmdL._SX427_BO1,204,203,200_.jpg",
+    "author":"Paulo Coulho",
+    "price":"550"
+  }
+
+]
 
 }
